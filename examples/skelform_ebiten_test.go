1b4a6042cbc62b8be75e12a7657f528fc09686e3
Retropaint <darkglasses1122@gmail.com>
2025-07-19 02:50:41 +0700

    initial commit

---
+++ examples/skelform_ebiten_test.go
@@ -0,0 +1,86 @@
+package skelform_ebiten
+
+import (
+	"image"
+	"log"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+
+	// "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/retropaint/skelform_go"
+	"github.com/retropaint/skelform_ebiten"
+)
+
+type Game struct {
+	root       skelform.Root
+	texture    image.Image
+	frame      int
+	player_pos skelform.Vec2
+	moving     bool
+	start_time time.Time
+}
+
+func (g *Game) Update() error {
+	var speed float32 = 5
+	g.moving = false
+
+	var keys []ebiten.Key
+	for _, key := range inpututil.AppendPressedKeys(keys) {
+		switch key {
+		case ebiten.KeyW:
+			g.player_pos.Y -= speed
+			g.moving = true
+		case ebiten.KeyS:
+			g.player_pos.Y += speed
+			g.moving = true
+		case ebiten.KeyA:
+			g.player_pos.X -= speed
+			g.moving = true
+		case ebiten.KeyD:
+			g.player_pos.X += speed
+			g.moving = true
+		}
+	}
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ao := skelform_ebiten.AnimOptions{}
+	ao.Init()
+
+	ao.Pos_offset = g.player_pos
+
+	anim_idx := 0
+	if g.moving {
+		anim_idx = 1
+	}
+
+	elapsed := time.Now().UnixMilli() - g.start_time.UnixMilli()
+	time_frame := skelform_ebiten.Get_frame_by_time(g.root.Armature.Animations[anim_idx], elapsed, false)
+	skelform_ebiten.Animate(screen, g.root.Armature, g.texture, anim_idx, time_frame, ao)
+	g.frame += 1
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 1280, 720
+}
+
+func main() {
+	ebiten.SetWindowSize(1280, 720)
+	ebiten.SetWindowTitle("Hello, World!")
+	root, texture := skelform_ebiten.Load("skellington.skf")
+	size_x, size_y := ebiten.WindowSize()
+	if err := ebiten.RunGame(&Game{
+		root:       root,
+		texture:    texture,
+		start_time: time.Now(),
+		player_pos: skelform.Vec2{
+			X: float32(size_x) / 2,
+			Y: float32(size_y) / 2,
+		},
+	}); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ skelform_ebiten.go
@@ -0,0 +1,89 @@
+package skelform_ebiten
+
+import (
+	"image"
+	"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/retropaint/skelform_go"
+)
+
+type AnimOptions struct {
+	Pos_offset   skelform.Vec2
+	Scale_factor float32
+}
+
+func (ao *AnimOptions) Init() {
+	ao.Pos_offset = skelform.Vec2{X: 0, Y: 0}
+	ao.Scale_factor = 0.25
+}
+
+func Animate(screen *ebiten.Image, armature skelform.Armature, texture image.Image, anim_idx int, frame int, anim_options AnimOptions) {
+	animated_bones := skelform.Animate(armature, anim_idx, frame)
+	tex := ebiten.NewImageFromImage(texture)
+
+	var props []skelform.Bone
+	for _, bone := range animated_bones {
+		props = append(props, bone)
+
+		prop := &bone
+
+		if prop.Tex_idx == -1 {
+			continue
+		}
+
+		// crop texture to this prop
+		tex_offset := skelform.Vec2{
+			X: armature.Textures[prop.Tex_idx].Offset.X,
+			Y: armature.Textures[prop.Tex_idx].Offset.Y,
+		}
+		tex_size := skelform.Vec2{
+			X: armature.Textures[prop.Tex_idx].Size.X,
+			Y: armature.Textures[prop.Tex_idx].Size.Y,
+		}
+		sub := tex.SubImage(image.Rectangle{
+			Min: image.Point{
+				X: int(tex_offset.X),
+				Y: int(tex_offset.Y),
+			},
+			Max: image.Point{
+				X: int(tex_offset.X) + int(tex_size.X),
+				Y: int(tex_offset.Y) + int(tex_size.Y),
+			},
+		})
+
+		op := &ebiten.DrawImageOptions{}
+
+		// Ebiten treats positive Y as down
+		prop.Pos.Y = -prop.Pos.Y
+
+		// center prop for scale & rot operations
+		size := skelform.Vec2{
+			X: armature.Textures[prop.Tex_idx].Size.X / 2 * prop.Scale.X,
+			Y: armature.Textures[prop.Tex_idx].Size.Y / 2 * prop.Scale.Y,
+		}
+		cos := math.Cos(float64(prop.Rot))
+		sin := math.Sin(float64(prop.Rot))
+		prop.Pos.X -= size.X*float32(cos) + size.Y*float32(sin)
+		prop.Pos.Y += size.X*float32(sin) - size.Y*float32(cos)
+
+		op.GeoM.Scale(float64(prop.Scale.X*anim_options.Scale_factor), float64(prop.Scale.Y*anim_options.Scale_factor))
+		op.GeoM.Rotate(-float64(prop.Rot))
+
+		final_pos := skelform.Vec2{
+			X: prop.Pos.X*anim_options.Scale_factor + anim_options.Pos_offset.X,
+			Y: prop.Pos.Y*anim_options.Scale_factor + anim_options.Pos_offset.Y,
+		}
+		op.GeoM.Translate(float64(final_pos.X), float64(final_pos.Y))
+
+		screen.DrawImage(sub.(*ebiten.Image), op)
+	}
+}
+
+func Get_frame_by_time(anim skelform.Animation, unix_milli int64, reverse bool) int {
+	return skelform.Get_frame_by_time(anim, unix_milli, reverse)
+}
+
+func Load(path string) (skelform.Root, image.Image) {
+	return skelform.Load(path)
+}
