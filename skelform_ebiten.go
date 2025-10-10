90a5849a44a26f9dd749c2951286269ebfdd6883
Retropaint <darkglasses1122@gmail.com>
2025-10-10 18:18:16 +0700

    simultaneous animation support

--- skelform_ebiten.go
+++ skelform_ebiten.go
@@ -11,29 +11,30 @@ import (
 )
 
 type AnimOptions struct {
-	Pos_offset   skelform_go.Vec2
-	Scale_factor float32
+	Position skelform_go.Vec2
+	Scale    float32
 }
 
 func (ao *AnimOptions) Init() {
-	ao.Pos_offset = skelform_go.Vec2{X: 0, Y: 0}
-	ao.Scale_factor = 0.25
+	ao.Position = skelform_go.Vec2{X: 0, Y: 0}
+	ao.Scale = 0.25
 }
 
 func Animate(
-	bones []skelform_go.Bone,
-	ikFamilies []skelform_go.IkFamily,
-	animation skelform_go.Animation,
+	armature *skelform_go.Armature,
+	animations []skelform_go.Animation,
+	frames []int,
 	screen *ebiten.Image,
-	frame int,
 	anim_options AnimOptions,
 ) []skelform_go.Bone {
 	var animatedBones []skelform_go.Bone
-	for _, bone := range bones {
+	for _, bone := range armature.Bones {
 		animatedBones = append(animatedBones, bone)
 	}
 
-	skelform_go.Animate(animatedBones, animation, frame)
+	for i := range animations {
+		skelform_go.Animate(animatedBones, animations[i], frames[i])
+	}
 
 	var inheritedBones []skelform_go.Bone
 	for _, bone := range animatedBones {
@@ -43,16 +44,16 @@ func Animate(
 	skelform_go.Inheritance(inheritedBones, make(map[uint]float32))
 	var ikRots map[uint]float32
 	for i := 0; i < 10; i++ {
-		ikRots = skelform_go.InverseKinematics(inheritedBones, ikFamilies)
+		ikRots = skelform_go.InverseKinematics(inheritedBones, armature.Ik_families)
 	}
 	skelform_go.Inheritance(animatedBones, ikRots)
 
 	for b := range animatedBones {
 		bone := &animatedBones[b]
-		bone.Scale = bone.Scale.Mul(skelform_go.Vec2{X: anim_options.Scale_factor, Y: anim_options.Scale_factor})
+		bone.Scale = bone.Scale.Mul(skelform_go.Vec2{X: anim_options.Scale, Y: anim_options.Scale})
 		bone.Pos.Y = -bone.Pos.Y
-		bone.Pos = bone.Pos.Mul(skelform_go.Vec2{X: anim_options.Scale_factor, Y: anim_options.Scale_factor})
-		bone.Pos = bone.Pos.Add(anim_options.Pos_offset)
+		bone.Pos = bone.Pos.Mul(skelform_go.Vec2{X: anim_options.Scale, Y: anim_options.Scale})
+		bone.Pos = bone.Pos.Add(anim_options.Position)
 	}
 
 	return animatedBones
